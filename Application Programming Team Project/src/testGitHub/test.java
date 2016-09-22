3dadcaf252febbbc55651bde543d00a25c57db82
cat barlow <cbarlow6@student.gsu.edu>
2016-09-22 18:05:21 -0400

    Initial Setup Team Project- GitHub

---
+++ Application Programming Team Project/src/testGitHub/test.java
@@ -0,0 +1,25 @@
+package testGitHub;
+
+//This program generates three integers and prompts the user to enter the sum
+
+import java.util.Scanner;
+
+public class test {
+
+public static void main(String [] args) {
+
+int number1 = (int)(Math.random() * 100);
+int number2 = (int)(Math.random() * 100);
+
+Scanner input = new Scanner(System.in);
+
+System.out.print("What is the sum of " + number1 + " + " + number2 + " ?:");
+
+int answer = input.nextInt();
+
+System.out.println(number1 + " + " + number2 + " = " + answer + " is " + 
+  (number1 + number2 == answer));
+
+input.close();
+}
+}
