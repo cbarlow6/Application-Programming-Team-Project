18d13f8733371238ca1a4754133b6e286f03632f
cat barlow <cbarlow6@student.gsu.edu>
2016-12-03 11:14:58 -0500

    update 20161203

--- Application Programming Team Project/src/teamProject/MainMenu.java
+++ Application Programming Team Project/src/teamProject/MainMenu.java
@@ -1,19 +1,19 @@
 package teamProject;
 
-import javafx.event.ActionEvent;
+//import javafx.event.ActionEvent;
 import javafx.application.Application;
-import javafx.event.EventHandler;
+//import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import javafx.scene.layout.VBox;
-import javafx.scene.control.Label;
+//import javafx.scene.layout.VBox;
+//import javafx.scene.control.Label;
 
 public class MainMenu extends Application {
 	
 	Stage window;
-	Scene mainMenu, loginPage;
+	//Scene mainMenu, loginPage;
 	
 	Button button;
 	
@@ -25,28 +25,16 @@ public class MainMenu extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
 		window = primaryStage;
+		window.setTitle("Main Menu");
 		
-		Label label1 = new Label("Main Menu");
-		Button button1 = new Button("Go to Login Page");
-		button1.setOnAction(e ->window.setScene(loginPage));
+		button = new Button("Click me");
+		button.setOnAction(e -> AlertBox.display("Register", "Registration Successful"));
 		
-		//Layout1
-		VBox  layout1 = new VBox(20);
-		layout1.getChildren().addAll(label1, button1);
-		mainMenu = new Scene(layout1, 600, 300);
-		
-		//Button 2
-		Button button2 = new Button("Return to Main Menu");
-		button2.setOnAction(e ->window.setScene(mainMenu));
-		
-		//Layout 2
-		StackPane layout2  = new StackPane();
-		layout2.getChildren().add(button2);
-		loginPage = new Scene(layout2, 600, 300);
-		
+		StackPane layout = new StackPane();
+		layout.getChildren().add(button);
+		Scene mainMenu = new Scene(layout, 300, 250);
 		
 		window.setScene(mainMenu);
-		window.setTitle("Main Menu");
 		window.show();
 		
 		
--- Application Programming Team Project/src/teamProject/SwitchScenes.java
+++ Application Programming Team Project/src/teamProject/SwitchScenes.java
@@ -4,9 +4,12 @@ package teamProject;
 
 //import javafx.event.ActionEvent;
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 //import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.scene.layout.VBox;
@@ -14,10 +17,20 @@ import javafx.scene.control.Label;
 
 public class SwitchScenes extends Application {
 	
+	//private static final Pos CENTER_LEFT = null;
 	Stage window;
-	Scene mainMenu, loginPage, registration;
+	Scene mainMenu, loginPage, register, customer, admin;
 	
 	Button button;
+	Button loginButton = new Button("Go to Login Page");
+	Button mainMenuButton = new Button("Return to Main Menu");
+	Button registerButton = new Button("Register");
+	Button customerButton = new Button("Customer");
+	Button adminButton = new Button("Admin");
+	Button returnMain = new Button("Return to Main Menu");
+	Button returnMain2 = new Button("Return to Main Menu");
+	Button returnMain3 = new Button("Return to Main Menu");
+	
 	
 	public static void main(String[] args){
 	launch(args);
@@ -28,44 +41,75 @@ public class SwitchScenes extends Application {
 		// TODO Auto-generated method stub
 		window = primaryStage;
 		
+		GridPane newgrid = new GridPane ();
+		newgrid.setPadding(new Insets (10, 10, 10, 10));
+		
+		//newgrid.setConstraints(adminButton, 0, 8);
+		
 		//Login Button
-		Label label1 = new Label("Main Menu");
-		Button loginbutton = new Button("Go to Login Page");
-		loginbutton.setOnAction(e ->window.setScene(loginPage));
+		Label mainMenuLabel = new Label("Main Menu");
+		//Button loginButton = new Button("Go to Login Page");
+		loginButton.setOnAction(e ->window.setScene(loginPage));
 		
 		//Main Menu Layout
-		VBox  layout1 = new VBox(20);
-		layout1.getChildren().addAll(label1, loginbutton);
-		mainMenu = new Scene(layout1, 600, 300);
+		VBox  mainMenuLayout = new VBox(20);
+		mainMenuLayout.getChildren().addAll(mainMenuLabel, loginButton);
+		mainMenu = new Scene(mainMenuLayout, 600, 300);
+		
+		//Return to Main Menu Button
+		//Label label1 = new Label("Main Menu");
+		//Button returnMain = new Button("Return to Main Menu");
+		returnMain.setOnAction(e ->window.setScene(mainMenu));
+		//Button returnMain2 = new Button("Return to Main Menu");
+		returnMain2.setOnAction(e ->window.setScene(mainMenu));
+		//Button returnMain3 = new Button("Return to Main Menu");
+		returnMain3.setOnAction(e ->window.setScene(mainMenu));
+				
+		//Registration Page Layout
+		VBox  registerLayout = new VBox(20);
+		Label registerLabel = new Label("Register");
+		registerLayout.getChildren().addAll(registerLabel, returnMain);
+		register = new Scene(registerLayout, 600, 300);
+		returnMain.setOnAction(e ->window.setScene(mainMenu));
+		
+		//Customer Page Layout
+		VBox  customerLayout = new VBox(20);
+		Label customerLabel = new Label("Customer");
+		customerLayout.getChildren().addAll(customerLabel, returnMain2);
+		customer = new Scene(customerLayout, 600, 300);
+		returnMain2.setOnAction(e ->window.setScene(mainMenu));
+				
+		//Admin Page Layout
+		VBox adminLayout = new VBox(20);
+		Label adminLabel = new Label("Admin");
+		adminLayout.getChildren().addAll(adminLabel, returnMain3);
+		admin = new Scene(adminLayout, 600, 300);
+		returnMain3.setOnAction(e ->window.setScene(mainMenu));
+		
 		
 		//Register and Main Menu Buttons
-		Label label2 = new Label("Login Page");
-		Button mainMenuButton = new Button("Return to Main Menu");
-		Button registerButton = new Button("Register");
+		Label loginLabel = new Label("Login Page");
+		//Button mainMenuButton = new Button("Return to Main Menu");
+		//Button registerButton = new Button("Register");
 		mainMenuButton.setOnAction(e ->window.setScene(mainMenu));
-		registerButton.setOnAction(e ->window.setScene(registration));
+		registerButton.setOnAction(e ->window.setScene(register));
+		customerButton.setOnAction(e ->window.setScene(customer));
+		adminButton.setOnAction(e ->window.setScene(admin));
+		
+		
 		
 		//Login Page Layout
-		StackPane layout2  = new StackPane();
-		layout2.getChildren().addAll(label2, mainMenuButton, registerButton);
-		loginPage = new Scene(layout2, 600, 300);
-		
-		
-		//Registration Button
-		/*Label label3 = new Label("Register");
-		Button returnMainMenu = mainMenuButton;
-		returnMainMenu.setOnAction(e ->window.setScene(mainMenu));
-		
-		//Register Page Layout
-		StackPane layout3  = new StackPane();
-		layout3.getChildren().addAll(label3, mainMenuButton, registerButton);
-		registration = new Scene(layout2, 600, 300);*/
-		
-		
-		
+		StackPane loginLayout  = new StackPane();
+		loginLayout.getChildren().addAll(loginLabel, mainMenuButton, registerButton, customerButton, adminButton);
+		StackPane.setAlignment(loginLabel, Pos.TOP_CENTER);
+		StackPane.setAlignment(mainMenuButton, Pos.TOP_RIGHT);
+		StackPane.setAlignment(registerButton, Pos.CENTER);
+		StackPane.setAlignment(customerButton, Pos.CENTER_RIGHT);
+		StackPane.setAlignment(adminButton, Pos.CENTER_LEFT);
+		loginPage = new Scene(loginLayout, 600, 300);
 			
 		window.setScene(mainMenu);
-		window.setTitle("Main Menu");
+		window.setTitle("Flight Reservation System");
 		window.show();
 			
 		/*primaryStage.setTitle("Flight");
